d2038dd25c4feafb4983317f04c8e60f2e70fccb
manas <manasvini569@revature.net>
2025-12-15 23:37:08 -0600

    Dockerfile and docker-compose

---
+++ manudockercompose/src/main/java/com/example/manudockercompose/ManudockercomposeApplication.java
@@ -0,0 +1,13 @@
+package com.example.manudockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManudockercomposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManudockercomposeApplication.class, args);
+	}
+
+}
